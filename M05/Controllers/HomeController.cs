568f425e13d9485264547590b27f84003a8c74bb
Marcos Robles <marcos92robles@gmail.com>
2020-03-06 10:46:16 -0600

    Add project files.

---
+++ M05/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M05.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            IEnumerable<String> Dias = new string[] { "Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","",""};
+            ViewBag.Dias = Dias;
+
+            return View();
+        }
+    }
+}
---
+++ M05/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M05.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var Context = new Models.NORTHWNDEntities();
+
+            var Products = Context.Products.ToList();
+
+            return View(Products);
+        }
+
+        public ActionResult Display(int ID)
+        {
+            var Context = new Models.NORTHWNDEntities();
+            var Product = Context.Products.FirstOrDefault(p => p.ProductID == ID);
+            return View("DinamycView",Product);
+        }
+
+
+        public ActionResult DisplayExtern(int ID)
+        {
+            var Extern = new Models.ExternProduct { ProductName = "ProductoExterno", UnitPrice = 10, UnitsInStock = 100, ExternID = 50 };
+
+            return View("DinamycView", Extern);
+        }
+    }
+}
